c43a1f5de95d1136b0d3aa4a0e7c59e2d39503c1
mnebuerquo <sherman.adelson@gmail.com>
2017-12-12 14:26:55 -0500

    added some notes in the python file

--- keras-tut.py
+++ keras-tut.py
@@ -1,3 +1,5 @@
+# ML Keras Tutorial
+# Using my virtualenv run script (./run keras-tut.py)
 # https://elitedatascience.com/keras-tutorial-deep-learning-in-python
 
 # 3. Import libraries and modules
@@ -15,7 +17,7 @@ from keras.layers import Convolution2D, MaxPooling2D
 from keras.utils import np_utils
 
 # for plotting a digit
-import matplotlib as mpl 
+import matplotlib as mpl
 mpl.use('TkAgg') # to run on mac in virtualenv
 from matplotlib import pyplot as plt
 
@@ -38,32 +40,32 @@ X_train = X_train.astype('float32')
 X_test = X_test.astype('float32')
 X_train /= 255
 X_test /= 255
- 
+
 # 6. Preprocess class labels
 Y_train = np_utils.to_categorical(y_train, 10)
 Y_test = np_utils.to_categorical(y_test, 10)
- 
+
 # 7. Define model architecture
 model = Sequential()
- 
+# This is as far as I got. At 49 I have an uncaught exception.
 model.add(Convolution2D(32, 3, 3, activation='relu', input_shape=(1,28,28)))
 model.add(Convolution2D(32, 3, 3, activation='relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Dropout(0.25))
- 
+
 model.add(Flatten())
 model.add(Dense(128, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(10, activation='softmax'))
- 
+
 # 8. Compile model
 model.compile(loss='categorical_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
- 
+
 # 9. Fit model on training data
-model.fit(X_train, Y_train, 
+model.fit(X_train, Y_train,
           batch_size=32, nb_epoch=10, verbose=1)
- 
+
 # 10. Evaluate model on test data
 score = model.evaluate(X_test, Y_test, verbose=0)
